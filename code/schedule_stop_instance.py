9f64343c53b1e98d6311a7b83f5227fcfa690445
Hung Cao <41882267+cycfsabo@users.noreply.github.com>
2021-04-02 21:58:19 +0700

    Add files via upload

---
+++ code/schedule_stop_instance.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+client = boto3.client('events')
+arn = 'arn:aws:lambda:us-east-2:700712043529:function:stop_instances'
+
+def lambda_handler(event, context):
+    # TODO implement
+    try:
+        value = list(event["queryStringParameters"]["text"].split(","))
+        rule_name = value[0]
+        cron_expression = value[1]
+        create_rule(rule_name,cron_expression)
+        response = create_target(rule_name,arn)
+        message = 'Created rule ' + rule_name + ' with target is Lambda function: stop_instances'
+        return {
+            'body': message 
+        }
+    except Exception as e:
+        message = 'An exception occurred: ' + format(e)
+        return {
+            'body': message
+        }
+
+def create_rule(name,cron_expression):
+    response = client.put_rule (
+        Name=name,
+        ScheduleExpression=cron_expression,
+        State='ENABLED'
+    )
+    return response
+
+def create_target(rule_name, arn):
+    response = client.put_targets(
+        Rule=rule_name,
+        Targets=[
+            {
+                'Id':'hungcao19',
+                'Arn':arn
+            }]
+        )
+    return response
+    
